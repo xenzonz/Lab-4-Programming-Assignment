d51fc1207104774342ae4a2f553742688141d91b
sam cocquyt <samcocquyt04@gmail.com>
2026-02-08 05:32:28 -0500

    docstring added

--- Lab4_xenzonz-2.py
+++ Lab4_xenzonz-2.py
@@ -1,1 +1,9 @@
-print("hello world")
+"""
+Docstring for Lab4_xenzonz_2
+i. LAB 4: Guess the Number - Hangman Style
+ii. Sam Cocquyt
+iii. Player tries to guess a random number between 1 to 15. Each incorrect guess reveals another part of a hangman drawing.
+Will also give feedback if guess is too high or too low
+iv. No starter code
+v. 2/8/2026
+"""
